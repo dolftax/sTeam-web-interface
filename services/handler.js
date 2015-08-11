8ce1dd91ba8fdce41758b6b52b57bfa6086f946f
Jaipradeesh <jaipradeesh@gmail.com>
2015-08-12 01:04:40 +0530

    CSS made local. CDN took long time
    Signed-off-by: Jaipradeesh <jaipradeesh@gmail.com>

--- services/handler.js
+++ services/handler.js
@@ -122,7 +122,6 @@ angular.module('steam')
       } else if (handler.loginp() && !next.requireLogin) {
         event.preventDefault()
         $state.go('workarea.list')
-        localStorageService.set('baseurl', config.baseurl)
       }
     })
   }])
