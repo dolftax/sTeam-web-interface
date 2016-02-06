eaf1dd61067eb2fe5559688961d1639dd7d57732
AkhilHector <akhilhector.1@gmail.com>
2016-02-06 14:21:59 +0530

    Fixes #37, The document/room controller can now POST requests to the server
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- controllers/workareaCtrl.js
+++ controllers/workareaCtrl.js
@@ -1,11 +1,17 @@
 angular.module('steam')
 
-  .controller('optionsCtrl', ['$state', '$scope', '$modal', 'localStorageService', 'handler',
-    function ($state, $scope, $modal, localStorageService, handler) {
-    $scope.createdoc = function () {
+  .controller('optionsCtrl', ['$state', '$scope', '$modal', '$location', 'localStorageService', 'handler',
+    function ($state, $scope, $modal, $location, localStorageService, handler) {
+    $scope.createdoc = function (docDetails) {
+      $scope.docDetails = docDetails;
       $modal.open({
         templateUrl: 'views/createdoc.html',
-        controller: 'createDocCtrl'
+        controller: 'createDocCtrl',
+        resolve: {
+          docDetails: function () {
+            return $scope.docDetails;
+          }
+        }
       })
     }
     $scope.createroom = function () {
@@ -30,6 +36,81 @@ angular.module('steam')
 
   .controller('commentsCtrl', ['$scope', function ($scope) {}])
 
-  .controller('createDocCtrl', ['$scope', function ($scope) { }])
+  .directive('fileModel', ['$parse', function ($parse) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        var model = $parse(attrs.fileModel);
+        var modelSetter = model.assign;
 
-  .controller('createRoomCtrl', ['$scope', function ($scope) {}])
+        element.bind('change', function(){
+          scope.$apply(function(){
+            modelSetter(scope, element[0].files[0]);
+          });
+        });
+      }
+    }
+  }])
+
+  .service('fileUpload', ['$rootScope', '$http', function ($rootScope, $http) {
+    this.uploadFileToUrl = function(file, uploadUrl){
+      var fd = new FormData();
+      $rootScope.loading = true;
+      $rootScope.uploadComplete = false;
+      fd.append('file', file);
+      $http.post(uploadUrl, fd, {
+        transformRequest: angular.identity,
+        headers: {'Content-Type': undefined}
+      })
+      .success(function (){
+        $rootScope.loading = false;
+        $rootScope.uploadComplete = true;
+        console.log("it is done");
+      })
+      .error(function (){
+        console.log(status);
+        $rootScope.loading = false;
+        $rootScope.uploadComplete = false;
+      });
+    }
+  }])
+
+  .controller('createDocCtrl', ['$scope', '$location', '$modalInstance', 'fileUpload',
+    function ($scope, $location, $modalInstance, fileUpload) {
+      $scope.uploadFile = function (){
+        var file = $scope.myFile;
+        var uploadUrl = $location.path();
+        console.log('file is ' );
+        console.dir(file);
+        fileUpload.uploadFileToUrl(file, uploadUrl);
+      }
+      $scope.submit = function () {
+        // body...
+      }
+      $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+      }
+  }])
+
+  .controller('documentCtrl', ['$scope', 'PDFViewerService', function ($scope, pdf) {
+    $scope.instance = pdf.Instance("viewer");
+    $scope.prevPage = function () {
+      $scope.instance.prevPage();
+    }
+    $scope.nextPage = function () {
+      $scope.instance.nextPage();
+    }
+    $scope.gotoPage = function (page) {
+      $scope.intance.gotoPage(page);
+    }
+    $scope.pageLoaded = function (currentPage, totalPages) {
+      $scope.currentPage = currentPage;
+      $scope.totalPages = totalPages;
+    }
+  }])
+
+  .controller('createRoomCtrl', ['$scope', '$modalInstance', function ($scope, $modalInstance) {
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    }
+  }])
